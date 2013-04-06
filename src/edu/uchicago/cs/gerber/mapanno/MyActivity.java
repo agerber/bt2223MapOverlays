de48eab4cf090869e5e54fb3460266a212c5b14a
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 20:08:27 -0500

    p0

--- src/edu/uchicago/cs/gerber/mapanno/MyActivity.java
+++ src/edu/uchicago/cs/gerber/mapanno/MyActivity.java
@@ -21,72 +21,16 @@ import com.google.android.maps.MapView;
 //from www.apress.com/9781430234135/
 //modified and refactored by Adam Gerber, University of Chicago
 //icons set to approx 50px x 50px
-public class MyActivity extends MapActivity implements OnCheckedChangeListener {
-
-	//UI elements
-    MapView map;
-    ToggleButton tglView;
-    
-    
-    MapController controller;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        tglView = (ToggleButton) findViewById(R.id.tglView  );
-        tglView.setOnCheckedChangeListener(this);
-        
- 
-        
-        map = (MapView)findViewById(R.id.map);
-        controller = map.getController();
-        
-        ArrayList<GeoPoint> gptLocations = new ArrayList<GeoPoint>();
-        ArrayList<Drawable> drwImages = new ArrayList<Drawable>();
-        
-        
-        //Quad U of C Hyde Park Campus: Latitude: 41789756, Longitude: -87599771
-        gptLocations.add(new GeoPoint(41789756,-87599771));
-        drwImages.add(getResources().getDrawable(R.drawable.uc));
-        
-        //medici cafe.
-        //http://geocoder.us/demo.cgi?address=1327+E+57th+St%2C+Chicago%2C+IL+60637
-        gptLocations.add(new GeoPoint(41791439, -87593357));
-        drwImages.add(getResources().getDrawable(R.drawable.gargoyle));
-
-        LocationOverlay lovOverlay = new LocationOverlay(this, getResources().getDrawable(R.drawable.uc));
-        lovOverlay.setItems(gptLocations, drwImages);
-        map.getOverlays().add(lovOverlay);
-        controller.setCenter(gptLocations.get(0));
-        controller.setZoom(15);
-        
-        
-        map.setSatellite(false);
-        map.setBuiltInZoomControls(true);
-        //too bad this is deprecated.
-        // map.setStreetView(true);
-        
-    }
-    
-    
-    //not used but requird 
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
-    
-
+public class MyActivity extends MapActivity {
 
 	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
-		if(isChecked){
-			map.setSatellite(true);
-		} else {
-			map.setSatellite(false);
-		}
-		
+		return false;
 	}
+
+
+
+        
+       
 }
